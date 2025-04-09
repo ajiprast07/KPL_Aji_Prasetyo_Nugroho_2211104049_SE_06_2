c3fa9db554b1715369d2c82a0dde2fbd8b40f596
Aji Prasetyo Nugroho <136548622+ajiprast07@users.noreply.github.com>
2025-04-09 23:33:39 +0700

    05_Generics

---
+++ 05_Generics/GUIDED/class.js
@@ -0,0 +1,18 @@
+class GenericList {
+    constructor() {
+        this.items = [];
+    }
+ 
+    add(item) {
+        this.items.push(item);
+    }
+ 
+    getAll() {
+        return this.items;
+    }
+}
+ 
+const list = new GenericList();
+list.add(1);
+list.add("Hello");
+console.log(list.getAll());

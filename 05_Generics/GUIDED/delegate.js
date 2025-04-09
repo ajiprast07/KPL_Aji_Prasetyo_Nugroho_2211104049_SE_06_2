c0127ef92505f60156513d0bfa62ce5ccb2cef9d
Aji Prasetyo Nugroho <136548622+ajiprast07@users.noreply.github.com>
2025-04-09 23:36:01 +0700

    Delete 05_Generics/GUIDED directory

--- 05_Generics/GUIDED/delegate.js
+++
@@ -1,5 +0,0 @@
-function genericDelegate(callback, value) {
-    callback(value);
-}
- 
-genericDelegate(console.log, "Event Triggered");
--- 05_Generics/GUIDED/function.js
+++
@@ -1,7 +0,0 @@
-function swap(a, b) {
-    return [b, a];
-}
- 
-let x = 5, y = 10;
-[x, y] = swap(x, y);
-console.log(x, y); // Output: 10, 5
